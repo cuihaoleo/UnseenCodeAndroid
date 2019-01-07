9b179e95dad7cc7e96929de38a83803d4ee68a97
CUI Hao (MSYS2) <cuihao.leo@gmail.com>
2019-01-07 15:42:30 +0800

    weekend update

--- app/src/main/java/com/example/cuihao/unseencodeandroid/MainActivity.kt
+++ app/src/main/java/com/example/cuihao/unseencodeandroid/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import io.fotoapparat.Fotoapparat
 import io.fotoapparat.configuration.CameraConfiguration
 import io.fotoapparat.facedetector.Rectangle
@@ -12,8 +13,9 @@ import io.fotoapparat.log.logcat
 import io.fotoapparat.parameter.ScaleType
 import io.fotoapparat.preview.Frame
 import io.fotoapparat.result.BitmapPhoto
-import io.fotoapparat.selector.back
+import io.fotoapparat.selector.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.imgproc.Imgproc
@@ -23,8 +25,14 @@ import java.io.File
 import org.opencv.core.*
 import org.opencv.core.MatOfPoint2f
 import org.opencv.core.Mat
-import kotlin.text.StringBuilder
+import java.util.*
 
+const val DEST_WIDTH = 1200
+const val BLOCK_N = 17
+const val MLEN = 511
+const val CLEN = (BLOCK_N * BLOCK_N) + (BLOCK_N - 1) * (BLOCK_N - 1)
+const val PREFIX_LEN = (CLEN - MLEN) / 2
+const val POSTFIX_LEN = (CLEN - MLEN) - PREFIX_LEN
 
 class MainActivity : Activity(), View.OnClickListener {
 
@@ -32,7 +40,9 @@ class MainActivity : Activity(), View.OnClickListener {
 
     val configuration by lazy {
         CameraConfiguration(
-            frameProcessor = this::processPreview
+            frameProcessor = this::processPreview,
+            jpegQuality = highestQuality(),
+            sensorSensitivity = highestSensorSensitivity()
         )
     }
 
@@ -77,49 +87,25 @@ class MainActivity : Activity(), View.OnClickListener {
             180 -> Core.rotate(bgr, bgr, Core.ROTATE_180)
             270 -> Core.rotate(bgr, bgr, Core.ROTATE_90_CLOCKWISE)
         }
-        saveImage("image.png", bgr)
-
+        /*saveImage("image.png", bgr)
         Log.d(TAG,"PREVIEW: ${bgr.height()} x ${bgr.width()}")
-        val buf = detectBoxJNI(bgr.nativeObjAddr) ?: return
 
-        val pts = Array(4) { Point(buf[it*2], buf[it*2+1]) }
-
-        val rects = Array(4) {
-            Rectangle(
-                (pts[it].x / bgr.cols()).toFloat() - 0.01F,
-                (pts[it].y / bgr.rows()).toFloat() - 0.01F,
-                0.02F, 0.02F
-            )
-        }
-
-        this.runOnUiThread{ rectangles_view.setRectangles(rects.asList()) }
-
-        val BORDER_WIDTH = 14
-        val TARGET_SIZE = 512
-
-        val transformed = Mat(TARGET_SIZE + BORDER_WIDTH*2, TARGET_SIZE + BORDER_WIDTH*2, CvType.CV_8UC3)
-        val src_mat = MatOfPoint2f()
-        src_mat.fromList(pts.asList())
-        val dst_mat = MatOfPoint2f(
-            Point(0.0, 0.0),
-            Point(transformed.width() - 1.0, 0.0),
-            Point(transformed.width() - 1.0, transformed.height() - 1.0),
-            Point(0.0, transformed.height() - 1.0)
-        )
-
-        val matrix = Imgproc.getPerspectiveTransform(src_mat, dst_mat)
-        Imgproc.warpPerspective(bgr, transformed, matrix, transformed.size())
-
-        val roi = Mat(transformed, Rect(BORDER_WIDTH, BORDER_WIDTH, TARGET_SIZE, TARGET_SIZE))
+        val pts = detectBox(bgr) ?: return
+        val roi = correctImage(bgr, pts)
+        val msg = decodeUnseenCode(roi)
         saveImage("corr.png", roi)
+        Log.d(TAG, "MSG: $msg")*/
 
-        val msg_bits = decodeJNI(roi.nativeObjAddr) ?: return;
-        val msg_str = StringBuilder()
-        msg_bits.forEach {
-            msg_str.append(it);
+        val pts = detectBox(bgr) ?: return
+        this.runOnUiThread{
+            rectangles_view.setRectangles(List(4) {
+                Rectangle(
+                    (pts[it].x / bgr.cols()).toFloat() - 0.01F,
+                    (pts[it].y / bgr.rows()).toFloat() - 0.01F,
+                    0.02F, 0.02F
+                )
+            })
         }
-
-        Log.d(TAG, "MSG: $msg_str")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -144,27 +130,125 @@ class MainActivity : Activity(), View.OnClickListener {
         fotoapparat.stop()
     }
 
+    fun collectPhoto() {
+        doAsync {
+            val dup = 9
+            val photos = Array(dup) {
+                Log.d(TAG,"Photo $it go...")
+                val photoResult = fotoapparat.takePicture()
+                val bitmapPhoto = photoResult.toBitmap().await()
+                Log.d(TAG,"Photo $it captured!")
+                sample_text.text = "Photo $it captured!"
+                bitmapPhoto
+            }
+
+            val vote = Array(CLEN){ 0 }
+            fotoapparat.stop()
+            photos.forEachIndexed{ index, bitmapPhoto -> Int
+                val rgba = Mat()
+                val bgr = Mat()
+                val bmp32 = bitmapPhoto?.bitmap?.copy(Bitmap.Config.ARGB_8888, true)
+                bmp32 ?: throw RuntimeException("BAD BITMAP")
+                Utils.bitmapToMat(bmp32, rgba)
+                Imgproc.cvtColor(rgba, bgr, Imgproc.COLOR_RGBA2BGR)
+
+                when (bitmapPhoto.rotationDegrees) {
+                    90 -> Core.rotate(bgr, bgr, Core.ROTATE_90_COUNTERCLOCKWISE)
+                    180 -> Core.rotate(bgr, bgr, Core.ROTATE_180)
+                    270 -> Core.rotate(bgr, bgr, Core.ROTATE_90_CLOCKWISE)
+                }
+
+                val pts = detectBox(bgr) ?: return@forEachIndexed
+                val roi = correctImage(bgr, pts)
+                saveImage("corr_$index.png", roi)
+                sample_text.text = "Photo $index processed!"
+                val msg = decodeUnseenCode(roi)
+                vote.indices.forEach{
+                    vote[it] += if (msg[it]) 1 else 0
+                }
+            }
+
+            val finalMessage = vote.map{ if (it * 2 > dup) 1.toByte() else 0.toByte() }
+            finalMessage.subList(PREFIX_LEN, PREFIX_LEN + MLEN)
+            val msg = unseenCodeDecodeJNI(finalMessage.toByteArray())
+            sample_text.text = msg
+            fotoapparat.start()
+        }
+    }
+
     override fun onClick(v: View?) {
-        val photoResult = fotoapparat.takePicture()
+        collectPhoto()
+        /*val photoResult = fotoapparat.takePicture()
         photoResult
             .toBitmap()
             .whenAvailable { bitmapPhoto: BitmapPhoto? ->
-                val mat = Mat()
+                val rgba = Mat()
+                val bgr = Mat()
                 val bmp32 = bitmapPhoto?.bitmap?.copy(Bitmap.Config.ARGB_8888, true)
                 bmp32 ?: throw RuntimeException("BAD BITMAP")
-                Utils.bitmapToMat(bmp32, mat);
-                Log.d(TAG, "${mat.height()} x ${mat.width()}")
-            }
+                Utils.bitmapToMat(bmp32, rgba)
+                Imgproc.cvtColor(rgba, bgr, Imgproc.COLOR_RGBA2BGR)
+
+                when (bitmapPhoto.rotationDegrees) {
+                    90 -> Core.rotate(bgr, bgr, Core.ROTATE_90_COUNTERCLOCKWISE)
+                    180 -> Core.rotate(bgr, bgr, Core.ROTATE_180)
+                    270 -> Core.rotate(bgr, bgr, Core.ROTATE_90_CLOCKWISE)
+                }
+
+                //saveImage("big_image.png", bgr)
+                val pts = detectBox(bgr) ?: return@whenAvailable
+                val roi = correctImage(bgr, pts)
+                val msg = decodeUnseenCode(roi)
+                //saveImage("big_corr_$count.png", roi)
+                Log.d(TAG, "BIG_MSG: $msg")
+            }*/
     }
 
-    private external fun stringFromJNI(): String
-    private external fun decodeJNI(addr: Long): ByteArray?
+    private external fun stringFromJNI(): String?
+    private external fun decodeJNI(addr: Long): ByteArray
     private external fun detectBoxJNI(addr: Long): DoubleArray?
+    private external fun unseenCodeDecodeJNI(arr: ByteArray): String?
+
+    private fun detectBox(bgr: Mat): Array<Point>? {
+        val buf = detectBoxJNI(bgr.nativeObjAddr) ?: return null
+        return Array(4) { Point(buf[it*2], buf[it*2+1]) }
+    }
+
+    private fun correctImage(bgr: Mat, pts: Array<Point>): Mat {
+        if (pts.size != 4) {
+            throw RuntimeException("pts.size != 4")
+        }
+
+        val transformed = Mat(TARGET_SIZE + BORDER_WIDTH*2, TARGET_SIZE + BORDER_WIDTH*2, CvType.CV_8UC3)
+        val src_mat = MatOfPoint2f()
+        src_mat.fromList(pts.asList())
+        val dst_mat = MatOfPoint2f(
+            Point(0.0, 0.0),
+            Point(transformed.width() - 1.0, 0.0),
+            Point(transformed.width() - 1.0, transformed.height() - 1.0),
+            Point(0.0, transformed.height() - 1.0)
+        )
+
+        val matrix = Imgproc.getPerspectiveTransform(src_mat, dst_mat)
+        Imgproc.warpPerspective(bgr, transformed, matrix, transformed.size())
+
+        return Mat(transformed, Rect(BORDER_WIDTH, BORDER_WIDTH, TARGET_SIZE, TARGET_SIZE))
+    }
+
+    private fun decodeUnseenCode(bgr: Mat): BitSet {
+        val msg_bits = decodeJNI(bgr.nativeObjAddr)
+        val ret = BitSet(msg_bits.size)
+        msg_bits.indices.forEach{ ret[it] = (msg_bits[it] != 0.toByte()) }
+        return ret
+    }
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
         }
+
+        private const val BORDER_WIDTH = 14
+        private const val TARGET_SIZE = 512
     }
 }
